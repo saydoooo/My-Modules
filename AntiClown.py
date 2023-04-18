e0f15f224360e5d5d0a63a8ada2eda53003a22e7
saydoooo <85806199+saydoooo@users.noreply.github.com>
2023-04-18 18:27:25 +0500

    Update AntiClown.py

--- AntiClown.py
+++ AntiClown.py
@@ -1,23 +1,20 @@
 # ---------------------------------------------------------------------------------
-#  /\_/\  🌐 This module was loaded from GitHub
-# ( o.o )  🔐 Licensed under the CC BY-NC-ND 4.0.
-#  > ^ <   ⚠️ Owner of romaterabyte doesn't take any responsibilities or intellectual property rights regarding this script
+#  /\_/\
+# ( o.o )  🔻 Модуль AntiClown
+#  > ^ <   🔐 Лицензировано под CC BY-NC-ND 4.0
 # ---------------------------------------------------------------------------------
-# Name                                           : AntiClown
-# Description                                    : A Telegram module that sends a specific sticker as a response to a clown reaction.
-# Author                                         : romaterabyte
-# Commands                                       :
-# .setsticker | .setchat 
+# Название                                       : AntiClown
+# Описание                                       : Этот модуль отправляет определенный стикер при постановке реакции "клоун" на сообщение
+# Автор                                          : D4n13l3k00
+# Команды                                        : .setsticker
+# Загружено из                                   : github.com
 # ---------------------------------------------------------------------------------
 
+from telethon import events
 from telethon.sync import TelegramClient
-from telethon.events import NewMessage, ReactionUpdate
-from telethon.tl.types import PeerUser, PeerChat, PeerChannel
-from telethon.tl.functions.messages import GetStickerSetRequest, GetEmojiKeywordsLanguageRequest, GetEmojiKeywordsRequest
-from telethon.tl.functions.messages import GetEmojiKeywordsDifferenceRequest, SetEmojiKeywordsLanguageRequest
-from telethon.tl.functions.messages import SetEmojiKeywordsRequest
 
 from .. import loader, utils
+
 
 @loader.tds
 class AntiClownMod(loader.Module):
@@ -25,43 +22,17 @@ class AntiClownMod(loader.Module):
 
     def __init__(self):
         self.client = None
-        self.chat_id = None
         self.sticker_id = None
 
     async def client_ready(self, client, db):
         self.client = client
 
-    async def on_message(self, event):
-        pass
-
     async def on_reaction(self, event):
-        if self.chat_id is not None and self.sticker_id is not None:
-            if event.user_id != event.sender_id:
-                return
-
-            if event.is_reply and event.reply_to_msg_id and event.reply_to_msg_id == event.message_id:
-                chat = await event.get_chat()
-                if chat.id == self.chat_id and event.emoji == "🤡":
-                    # Отправляем стикер при постановке реакции "🤡" на сообщение
-                    await self.client.send_sticker(chat, self.sticker_id)
-
-    async def setchatcmd(self, m):
-        """.setchat <чат> - установить чат, в котором будет происходить отслеживание реакции клоуна"""
-        args = utils.get_args_raw(m)
-        if not args:
-            await m.edit("<b>Укажите айди чата!</b>")
-            return
-
-        try:
-            self.chat_id = int(args)
-        except ValueError:
-            await m.edit("<b>Неверный формат айди чата!</b>")
-            return
-
-        await m.edit(f"<b>Чат успешно установлен:</b> {args}")
+        if self.sticker_id is not None and event.reaction == "🤡":
+            await self.client.send_sticker(event.chat_id, self.sticker_id)
 
     async def setstickercmd(self, m):
-        """.setsticker <стикер> - установить стикер, который будет отправляться при постановке реакции клоуна"""
+        """.setsticker <стикер> - установить стикер, который будет отправляться при постановке реакции "клоун" на сообщение"""
         args = utils.get_args_raw(m)
         if not args:
             await m.edit("<b>Укажите айди стикера!</b>")
@@ -74,3 +45,4 @@ class AntiClownMod(loader.Module):
             return
 
         await m.edit(f"<b>Стикер успешно установлен:</b> {args}")
+
