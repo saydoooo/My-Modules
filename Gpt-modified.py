17d76973a18cacda0cef115d1d106111bad90495
saydoooo <85806199+saydoooo@users.noreply.github.com>
2023-04-23 01:50:20 +0500

    Update Gpt-modified.py

--- Gpt-modified.py
+++ Gpt-modified.py
@@ -154,9 +154,9 @@ class GPTMod(loader.Module):
             ]
     )
 
-    response = completion.choices[0].message.content
+        response = completion.choices[0].message.content
 
-    await utils.answer(message, f"<code>{response}</code>" + self.strings("quest_k").format(args=f"<code>{args}</code>"))
+        await utils.answer(message, f"<code>{response}</code>" + self.strings("quest").format(args=f"<code>{args}</code>"))
 
 
     @loader.command(ru_doc="<вопрос> - ваш вопрос к FuckGPT")
